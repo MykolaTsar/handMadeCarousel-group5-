039728ec36a3d4ff0c7b35a35f819c9ca97cbc80
MykolaTsar <mykola.tsar@gmail.com>
2023-02-26 23:54:13 +0200

    added new

---
+++ js/changing.js
@@ -0,0 +1,18 @@
+(function () {
+    /* const changing = document.querySelector(".plus");
+    changing.addEventListener("click", function (e) {
+        changing.classList.toggle("active");
+    }); */
+
+/*     const navLinks = document.querySelectorAll(".header__menu-item");
+    navLinks.forEach((navLink) => {
+        navLink.addEventListener("click", (_) => {
+            if (burgerButton.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                burgerButton.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+        }
+        );
+    }); */
+})();
